5f7b6f6806c3d86446c9c1b59618a79118a76e2a
Dmytro Kondratiuk <dmkndr@gmail.com>
2014-09-28 15:13:17 +0700

    written 2pc impl; fixed tests

--- src/main/scala/org/example/Coordinator.scala
+++ src/main/scala/org/example/Coordinator.scala
@@ -3,44 +3,68 @@ package org.example
 import scala.collection.mutable.ListBuffer
 import akka.actor.{Props, ActorRef, Actor}
 import scala.reflect._
+import akka.pattern._
+import akka.util.Timeout
+import scala.concurrent.ExecutionContext.Implicits.global
 
 case class ReqSeq[T](tid: String, rs: Seq[T])
 case class Req[T](req: T)
-abstract class Coordinator[T, TA: ClassTag] extends Actor {
-  def transactor(tid: String, rs: ReqSeq[T]) = context.actorOf(Props(classTag[TA].runtimeClass, rs), tid)
-  def receive = {
-    case ReqSeq(tid, rs) => //TODO ask forward
-  }
-}
 
 object Process
 trait Result
 object Success extends Result
 object Failure extends Result
-case class Commit[T](r: Req[T])
-case object Ack
-abstract class Transactor[T, P <: Processor[T]: ClassTag] extends Actor { //TODO add timeout
-  def rs: ReqSeq[T]
-  def processor(tid: String) = context.actorOf(Props[P])
-  def commit
-  def all(l: ListBuffer[Result]): Option[Result]
 
-  val stat = ListBuffer[Result]()
-  val ack = ListBuffer[Ack.type]()
+trait Operation[T] { def r: Req[T]; def isCommit: Boolean }
+case class Commit[T](r: Req[T]) extends Operation[T] { def isCommit = true}
+case class Rollback[T](r: Req[T]) extends Operation[T] { def isCommit = false}
+case object Ack
+import akka.util.Timeout
+import scala.concurrent.duration._
+
+abstract class Coordinator[T, TA: ClassTag] extends Actor {
+  val timeout = 5 seconds
+  implicit val askTimeout = Timeout(timeout)
+  def transactor(rs: ReqSeq[T]) = context.actorOf(Props(classTag[TA].runtimeClass, rs), rs.tid)
   def receive = {
-    case Process => //send info to new actors
-    case Success => //add stats
-    case Failure => //add stats
-    case Ack => //add ack
+    case rs: ReqSeq[T] => transactor(rs) ? Process pipeTo sender
+  }
+}
+
+abstract class Transactor[T, P <: Processor[T]: ClassTag] extends Actor {
+  def rs: ReqSeq[T]
+  var parent: ActorRef = _
+  def process(tid: String, r: Req[T]) = context.actorOf(Props[P]) ! r
+  def commit = parent ! Success
+  def rollback = parent ! Failure
+
+  def decision  = //analyze stats
+    if (rs.rs.size != votes.size) None
+    else if (votes.forall(_._2.isCommit)) Some(Commit[T](_))
+    else Some(Rollback[T](_))
+
+  val votes = ListBuffer[(ActorRef, Operation[T])]() //voting
+  val ack = ListBuffer[Ack.type]() //before final acknowledgment
+  def receive = {
+    case Process =>
+      context.system.scheduler.scheduleOnce(5 seconds, self, "Timeout")
+      for (r <- rs.rs) process(rs.tid, Req(r)); parent = sender
+    case r: Operation[T] => votes += sender -> r
+      for (d <- decision; (proc, req) <- votes) proc ! d(req.r)
+    case Ack => ack += Ack; if (ack.size == rs.rs.size)
+      if (decision.get(null).isCommit) commit else rollback
+    case "Timeout" => parent ! Failure; context stop self
   }
 }
 
 trait Processor[T] extends Actor {
-  def process(t: T): Result
+  def process(r: Req[T]): Operation[T]
   def complete(t: T)
+  def rollback(t: T)
   def receive = {
-    case r: Req[T] => //send success or failure
-    case a: Commit[T] => complete(a.r.req)
+    case r: Req[T] => sender ! process(r)
+    case a: Commit[T] => complete(a.r.req); sender ! Ack
+    case a: Rollback[T] => rollback(a.r.req); sender ! Ack
   }
 }
 
--- src/test/scala/org/example/CoordinatorTest.scala
+++ src/test/scala/org/example/CoordinatorTest.scala
@@ -1,54 +1,52 @@
 package org.example
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfter, FlatSpec, Matchers}
 import scala.concurrent._
 import akka.actor.{ActorSystem, Props}
-import scala.collection.mutable.ListBuffer
 import akka.pattern._
 import akka.util.Timeout
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.reflect._
 
 /**
  * Created by user on 9/28/14.
  */
-class CoordinatorTest extends FlatSpec with Matchers {
+case class Task(s: Result, isComplete: Promise[Result] = Promise[Result]())
+
+class CoordinatorImpl extends Coordinator[Task, TransactorImpl]
+
+class TransactorImpl(val rs: ReqSeq[Task]) extends Transactor[Task, ProcessorImpl]
+
+class ProcessorImpl extends Processor[Task] {
+  def process(r: Req[Task]) = if (r.req.s == Success) Commit(r) else Rollback(r)
+  def complete(t: Task) = t.isComplete.success(Success)
+  def rollback(t: Task) = t.isComplete.success(Failure)
+}
+
+class CoordinatorTest extends FlatSpec with Matchers with BeforeAndAfterAll {
   val system = ActorSystem("testSystem")
-  case class Task(s: Result, isComplete: Promise[Unit] = Promise[Unit]())
 
-  class CoordinatorImpl() extends Coordinator[Task, TransactorImpl]
-
-  class TransactorImpl(val rs: ReqSeq[Task]) extends Transactor[Task, ProcessorImpl] {
-    def all(candidates: ListBuffer[Result]) = ???
-    def commit = {sender ! Success}
-    def rollback = {sender ! Failure}
-  }
-
-  class ProcessorImpl extends Processor[Task] {
-    def process(t: Task) = t.s
-    def complete(t: Task) = t.isComplete.success(())
-    def rollback(t: Task) = t.isComplete.failure(new RuntimeException("!"))
-  }
-
-  val coordinator = system.actorOf(Props(classOf[CoordinatorImpl], null), "coordinator")
+  val coordinator = system.actorOf(Props(classOf[CoordinatorImpl]), "coordinator")
 
   import scala.concurrent.duration._
   val timeout = 5 seconds
   implicit val askTimeout = Timeout(timeout)
 
   "transactor" should "complete transaction" in {
-    val request = ReqSeq("100500", Seq(Task(Success), Task(Success), Task(Success)))
+    val request = ReqSeq("100800", Seq(Task(Success), Task(Success), Task(Success)))
     val result = coordinator ? request
     Await.result(result, timeout) should be (Success)
     val tasks = Future.sequence(request.rs.map(_.isComplete.future))
     Await.result(tasks, timeout) should be (Seq(Success, Success, Success))
-
   }
 
   "transactor" should "rollback transaction" in {
-    val request = ReqSeq("100500", Seq(Task(Success), Task(Success), Task(Failure)))
+    val request = ReqSeq("100600", Seq(Task(Success), Task(Success), Task(Failure)))
     val result = coordinator ? request
     Await.result(result, timeout) should be (Failure)
     val tasks = Future.sequence(request.rs.map(_.isComplete.future))
-    Await.result(tasks, timeout) should be (Seq(Success, Success, Failure))
+    Await.result(tasks, timeout) should be (Seq(Failure, Failure, Failure))
   }
+
+  override def afterAll = system.shutdown()
 }
